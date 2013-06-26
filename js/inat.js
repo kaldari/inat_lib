c1e5a8062a99ad09dc0da83a0eeeac64d205269f
Kyle Garsuta <kyle.katsuta@gmail.com>
2013-06-26 16:07:00 -0700

    Minor tweaks

--- js/inat.js
+++ js/inat.js
@@ -1,32 +1,34 @@
 /* File:            inat.js
  * Author:          Kyle Garsuta
  * Created:         19 Jun 2013
- * Last modified:   21 Jun 2013 by Kyle
- * PRE:             User has granted access and OAuth2 token is appended
- *                     on callback query string
- * POST:            OAuth 2.0 token is stored in 'inat_auth' cookie
  *
- * Description:     
- *  This program extracts the OAuth2 token from the callback
- *  query string and stores it in 'inat_auth' cookie
+ * Description: A library of functions used by the iNat library
  *
  *  See iNat API Reference: http://www.inaturalist.org/pages/api+reference
- *
- *  This program is in the early stages of development and serves as a rudimentary proof
- *  of concept.
  */
 
 
 function redirect(redirect_url){
+// A simple redirect function
+// POST: Redirect to specified url
+
   window.location = redirect_url;
 }
 
-function authenticate(client_id, redirect_url){
-  window.location = "https://www.inaturalist.org/oauth/authorize?client_id=" + client_id + 
-    "&redirect_uri=" + escape(redirect_url) + "&response_type=token";
+function authenticate(client_id, callback_url){
+// Authenticate app based on id and callback url
+// POST: Redirect to app authentication url
+
+  redirect( "https://www.inaturalist.org/oauth/authorize?client_id=" + client_id + 
+    "&redirect_uri=" + escape(callback_url) + "&response_type=token" );
 }
 
 function callback(redirect_url){
+// This function extracts the OAuth2 token from the callback
+// query string and stores it in 'inat_auth' cookie
+// PRE: OAuth2 token is appended on callback query string
+// POST: OAuth 2.0 token is stored in 'inat_auth' cookie
+
   // Parse the query string
   var queryString = location.hash.substring(14);
   var token = queryString.substring(0, queryString.length - 18);
