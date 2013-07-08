b9ecd7f723e4b3330b297f3a4e5af746fc3cd3d3
Kyle Garsuta <kyle.katsuta@gmail.com>
2013-07-08 14:02:15 -0700

    Still testing get obs

--- test/php/obs.php
+++ test/php/obs.php
@@ -10,6 +10,15 @@
 // Include global config
 // include_once "config.php";
 
+function get_content($url){
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_URL, $url);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+
 class obs {
 /*
  * 
@@ -22,13 +31,12 @@ class obs {
   // PRE: Valid obs id
   // POST: Obs data stored in class variables
   
-  $this->id = $id;
-  echo $this->id;
+  echo get_content("http://www.inaturalist.org/observations/$id.json");
   }
 }
 
 $obs = new obs();
-$obs->get(50);
+$obs->get(298762);
 
 
 
