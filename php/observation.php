0016042c06f58cf87c3e21e34a125018dda074c2
Kyle Garsuta <kyle.katsuta@gmail.com>
2013-07-08 19:59:44 -0700

    Added helper methods to observation class

--- php/observation.php
+++ php/observation.php
@@ -8,8 +8,6 @@
  */
 
 // Include global config
-// include_once "config.php";
-
 include_once "config.php";
 include_once "inat.php";
 
@@ -22,32 +20,36 @@ class observation {
   // Default constructor
     if($id != NULL) {
     // WITH id, fetch from iNat
-      $this->get($id);
+      $this->use_get($id);
     } else {
     // WITHOUT id, create from form
-    // This part needs to be implemented
-      echo 'no id - need to implement';
+      $this->use_form();
     }
-  }
-
-  public function get($id) {
-  // GETs an observation with the input id from inaturalist.org
-  // PRE: Valid obs id
-  // POST: Obs data stored in class variables
-
-  $this->data = json_decode( get_content("http://www.inaturalist.org/observations/$id.json") );
   }
 
   public function print_data() {
   // Print all class variables
-  
   foreach ($this->data as $key => $value) {
     echo "<b>$key</b> is $value</br>";
     }
   }
+
+  private function use_get($id) {
+  // Helper method to construct observation
+  // GETs an observation with the input id from inaturalist.org
+  // PRE: Valid obs id
+  // POST: Obs data stored in class variables
+    $this->data = json_decode( get_content("http://www.inaturalist.org/observations/$id.json") );
+  }
+
+  private function use_form() {
+  // Helper method to construct observation
+  // Constructs an observation using data from $_POST
+    echo 'no id - need to implement';
+  }
 }
 
-$obs = new observation();
+$obs = new observation(333);
 $obs->print_data();
 
 
