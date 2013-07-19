6956619b55323dea4ab2ce0263708ba6ccd3e3ec
Kyle Garsuta <kyle.katsuta@gmail.com>
2013-07-19 16:40:14 -0700

    Added tabs to species view

--- mvc/view/species_view.class.php
+++ mvc/view/species_view.class.php
@@ -31,22 +31,21 @@ class speciesView {
     // Construct all names list
     $all_names = "<ul>";
     foreach ($this->data["taxon_names"] as $key => $array) {
-      $name = $lexicon = "";
-      foreach ($array as $key => $value) {
-        if($key=="lexicon")
-          $lexicon = $value;
-        if($key=="name")
-          $name = $value;
-      }
+      $name = $array["name"];
+      $lexicon = $array["lexicon"];
       $all_names .= "<li>$name ($lexicon)</li>";
     } $all_names .= "</ul>";
-
+    
+    $common_name = $this->data["common_name"]["name"];
+    $id = $this->data["id"];
+    $range_kmz = urlencode("http://www.inaturalist.org/taxa/$id/range.kml");
+    
     return
     '<div class="page-container" id="view_species">
       
        <div class="container">
         <h2>' . $this->data["name"] . 
-          ' (' . $this->data["common_name"]["name"] . ')</h2>
+          ($common_name ? (' ('.$common_name.')') : '') . '</h2>
       </div>
 		
 		  <div class="container" id="species_profile">
@@ -62,10 +61,52 @@ class speciesView {
         </div>
       </div>
       
-      <div class="container" id="species_desc">
-        <h3>Description from Wikipedia</h3>' .
-        '<p>' . $this->data["wikipedia_summary"] . '</p>' .
-		  '</div>
+      <div class="container">
+        <script src="
+          http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>
+        <link rel="stylesheet" type="text/css"
+          href="http://jquerytools.org/media/css/standalone.css"/>
+
+        <link rel="stylesheet" type="text/css"
+            href="http://jquerytools.org/media/css/tabs-no-images.css"/>
+        <ul class="css-tabs">
+          <li><a id="t1" href="#tab1">Description</a></li>
+          <li><a id="t2" href="#tab2">Range map</a></li>
+          <li><a id="t3" href="#tab3">Tab 3</a></li>
+        </ul>
+
+        <div class="css-panes">
+          <div id="species_desc">' .
+            '<h3>Description from Wikipedia</h3>' .
+            '<p>' . $this->data["wikipedia_summary"] . '</p>' . 
+          '</div>
+
+          <div>
+            <div id="range_map">
+              <iframe width="875" height="350" frameborder="0" 
+              scrolling="no" marginheight="0" marginwidth="0"
+              src="https://maps.google.com/maps?q='. $range_kmz . '&amp;output=embed">
+              </iframe>
+		        </div>
+          </div>
+
+          <div>
+            Tab 3
+          </div>
+        </div>
+
+        <script>
+          $(function() {
+          $(".css-tabs:first").tabs(".css-panes:first > div");
+          });
+        </script>
+      </div>
+      
+      
+      
+      
+      
+ 
       
     </div>
     ';
